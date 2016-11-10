a865de057ed40f7e952950ca76850c7a9849c3ad
Erick Wendel Gomes da Silva <erick.silva@fcamara.com.br>
2016-11-10 16:21:19 -0200

    poc

---
+++ index.js
@@ -0,0 +1,22 @@
+var pdf = require('pdfkit');
+var fs = require('fs');
+var data = require('./data');
+var myDoc = new pdf;
+var Report = require('fluentreports').Report;
+
+
+var dados = data[0]; 
+var detailArray = [];
+for (var i = 0; i < dados.length; i++) {
+    detailArray.push([i, 48]);
+}
+
+ 
+// Create a Report   
+var rpt = new Report("Report.pdf")
+    .fontsize(7)
+    .margins(6)
+    .pageHeader(["Relatório"]) // Add a simple (optional) page Header... 
+    .data(data)    // Layout the report in a grid of 200px & 50px 
+    .detail(detailArray)     // Layout the report in a grid of 200px & 50px 
+    .render();
